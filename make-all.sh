4abd9b2ef4e9b02ecc099ba6bad9d1281d66c322
Apollo Hogan <apollo.hogan@gmail.com>
2025-02-22 22:03:38 -0500

    .

--- make-all.sh
+++ make-all.sh
@@ -1,17 +1,27 @@
-/usr/bin/bash
+#!/usr/bin/bash
 
 VERSION="1.0"
 
 # universal
 UNIVERSAL="usss-ohio-v${VERSION}--universal.love"
-cd usss-ohio/
-/usr/bin/zip -9 -r ../${UNIVERSAL} .
+cd src/
+/usr/bin/zip -9 -r ../distributions/${UNIVERSAL} .
 cd ..
 
 # windows
 WIN_DIR="usss-ohio-v${VERSION}--win32"
 /usr/bin/mkdir ./${WIN_DIR}
-/usr/bin/cat love-win32.exe ${UNIVERSAL} > ./${WIN_DIR}/usss-ohio-v${VERSION}.exe
-/usr/bin/cp ./love-win32/* ./${WINDIR}
-/usr/bin/zip -9 -r ./${WIN_DIR} ./${WIN_DIR}.zip
+/usr/bin/cat ./love-win32/love.exe ./distributions/${UNIVERSAL} \
+  > ./distributions/${WIN_DIR}/usss-ohio-v${VERSION}.exe
+/usr/bin/cp ./love-win32/license.txt ./distributions/${WIN_DIR}
+/usr/bin/cp ./love-win32/love.dll ./distributions/${WIN_DIR}
+/usr/bin/cp ./love-win32/lua51.dll ./distributions/${WIN_DIR}
+/usr/bin/cp ./love-win32/mpg123.dll ./distributions/${WIN_DIR}
+/usr/bin/cp ./love-win32/msvcp120.dll ./distributions/${WIN_DIR}
+/usr/bin/cp ./love-win32/msvcr120.dll ./distributions/${WIN_DIR}
+/usr/bin/cp ./love-win32/OpenAL32.dll ./distributions/${WIN_DIR}
+/usr/bin/cp ./love-win32/SDL2.dll ./distributions/${WIN_DIR}
+cd ./distributions
+/usr/bin/zip -9 -r ${WIN_DIR}.zip ./${WIN_DIR}
+cd ..
 
